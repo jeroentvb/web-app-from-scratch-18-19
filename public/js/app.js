23448bfe0669c34f98a23a63ae4bb5d1e6bb7bb5
jeroentvb <jeroentvb@hotmail.com>
2019-02-04 16:10:29 +0100

    Loaded data and applied a grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,38 @@
 /* global fetch */
-
 'use strict'
 
-console.log('init!')
+const container = document.getElementsById('content')
+const apiKey = 'DEMO_KEY'
+const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`
 
-const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY'
+function init () {
+  getData(url)
+}
 
-fetch(url)
-  .then(res => console.log(res.json()))
-  .catch(err => console.error(err))
+function getData (url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => render(data.photos))
+    .catch(err => console.error(err))
+}
+
+function render (data) {
+  // main.textContent = JSON.stringify(data, null, 4)
+  console.log(data)
+
+  let content = ''
+
+  data.forEach(item => {
+    console.log(item)
+    content += `<article>
+    <a href="${item.img_src}" target="_blank"><img src="${item.img_src}" alt="${item.img_src}"></a>
+    <p>Id: ${item.id}</p>
+    <p>Mars date of photo (sol): ${item.sol}</p>
+    <p>Earth date of photo: ${item.earth_date}</p>
+    </article>`
+  })
+
+  container.innerHTML = content
+}
+
+init()
