cef7bb3b2d4025143451f051f0a39d24a73e61c1
jeroentvb <jeroentvb@hotmail.com>
2019-02-06 14:55:07 +0100

    Made code modular & added form error checking

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,56 @@
-/* global fetch */
 'use strict'
 
-import { apiKey } from './modules/api-key.js'
-
-const container = document.getElementById('content')
+import { getData } from './modules/get-data.js'
 
 function init () {
+  const rovers = [
+    'curiosity',
+    'opportunity',
+    'spirit'
+  ]
+
   let sol = {
     select: document.getElementById('sol-select'),
     submit: document.getElementById('submit-sol')
   }
+
   sol.submit.addEventListener('click', e => {
     e.preventDefault()
-    getData(sol.select.value)
+    if (sol.select.value === '') {
+      sol.select.value = 1
+      getData(1, rovers)
+        .then(data => render(data, rovers))
+        .catch(err => console.error(err))
+    }
+    getData(sol.select.value, rovers)
+      .then(data => render(data, rovers))
+      .catch(err => console.error(err))
   })
-  getData(1)
-}
 
-function getData (sol) {
-  container.innerHTML = `<p>Loading...</p>`
-  fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${apiKey()}`)
-    .then(res => res.json())
-    .then(data => render(data.photos))
+  getData(4, rovers)
+    .then(data => render(data, rovers))
     .catch(err => console.error(err))
 }
 
-function render (data) {
-  if (data.length < 1) {
-    container.innerHTML = `<article>
-    <p>There were no pictures taken on this day</p>
-    </article>`
-    return
-  }
+function render (data, rovers) {
+  rovers.forEach((rover, i) => {
+    let content = `<h2>Rover ${rover}</h2>`
+    if (data[i].length < 1) {
+      content += `<p>There were no pictures taken by this rover for this sol.</p>`
+      document.getElementById(`rover${i}`).innerHTML = content
+      return
+    }
 
-  let content = ''
+    data[i].forEach(item => {
+      content += `<article>
+      <a href="${item.img_src}" target="_blank"><img src="${item.img_src}" alt="${item.img_src}"></a>
+      <p>Id: ${item.id}</p>
+      <p>Earth date of photo: ${item.earth_date}</p>
+      </article>`
+    })
 
-  data.forEach(item => {
-    content += `<article>
-    <a href="${item.img_src}" target="_blank"><img src="${item.img_src}" alt="${item.img_src}"></a>
-    <p>Id: ${item.id}</p>
-    <p>Mars date of photo (sol): ${item.sol}</p>
-    <p>Earth date of photo: ${item.earth_date}</p>
-    </article>`
+    document.getElementById(`rover${i}`).innerHTML = content
   })
-
-  container.innerHTML = content
 }
 
 init()
